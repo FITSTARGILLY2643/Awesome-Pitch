cbbbb7f56a1336f8b7f0a97833096f14dca1571d
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2021-06-12 13:15:01 +0300

    Define the create_app method that create the app configurations

--- app/__init__.py
+++ app/__init__.py
@@ -14,3 +14,16 @@ photos = UploadSet('photos',IMAGES)
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
