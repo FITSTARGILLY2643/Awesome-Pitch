b4dc391c3b889bf0a059feea280cd151227cbea2
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2021-06-12 13:12:57 +0300

    Create the instances of the imported modules

--- app/__init__.py
+++ app/__init__.py
@@ -5,3 +5,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
+photos = UploadSet('photos',IMAGES)
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
