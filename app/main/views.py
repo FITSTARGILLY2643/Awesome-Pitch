132f766ca6e875b128876310cb195d9ee55347e1
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2021-06-12 18:47:11 +0300

    Defined the profle and the update_profile view functions

--- app/main/views.py
+++ app/main/views.py
@@ -23,3 +23,33 @@ def index():
 
 
     return render_template('index.html',title = title, interview = interview_piches, product = product_piches, promotion = promotion_pitches)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    pitches_count = Pitch.count_pitches(uname)
+    user_joined = user.date_joined.strftime('%b %d, %Y')
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user,pitches = pitches_count,date = user_joined)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form = form)
